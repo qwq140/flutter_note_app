da9769e0792d28a1aeddd390addda93bdd42f36d
qwq140 <qwq140@gmail.com>
2022-12-20 15:37:34 +0900

    notes의 ViewModel 작성

---
+++ lib/presentation/notes/notes_event.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:note_app/domain/model/note.dart';
+
+part 'notes_event.freezed.dart';
+
+// notes 화면상에서 일어날 수 있는 이벤트 정의
+// 로딩, 삭제, 삭제 취소
+@freezed
+abstract class NotesEvent with _$NotesEvent {
+  const factory NotesEvent.loadNotes() = LoadNotes;
+  const factory NotesEvent.deleteNote(Note note) = DeleteNote;
+  const factory NotesEvent.restoreNote() = RestoreNote;
+}
