e2a4226a7d66ffc73edbb1ef437b3a03823bb8cc
qwq140 <qwq140@gmail.com>
2022-12-21 13:40:02 +0900

    di setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/di/provider_setup.dart';
 import 'package:note_app/presentation/notes/notes_screen.dart';
 import 'package:note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // 플랫폼 채널의 위젯 바인딩을 보장한다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final providers = await getProviders();
+
+  runApp(
+    MultiProvider(
+      providers: providers,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,9 +36,9 @@ class MyApp extends StatelessWidget {
                     foregroundColor: darkGray,
                   ),
           appBarTheme: Theme.of(context).appBarTheme.copyWith(
-            backgroundColor: darkGray,
-            elevation: 0,
-          ),
+                backgroundColor: darkGray,
+                elevation: 0,
+              ),
         ),
         home: const NotesScreen());
   }
